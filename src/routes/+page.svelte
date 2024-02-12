06bbd78ae86bf895cbceb2f39054c4b2c5d8b553
morde <mordechai.hadad01@gmail.com>
2024-02-12 11:24:28 +0200

    first commit nice

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { Marquee } from "@selemondev/svelte-marquee";
+    import "@selemondev/svelte-marquee/dist/style.css";
+</script>
+
+<div class="flex w-full flex-col md:flex-row justify-center">
+    <div class="flex w-full max-w-5xl">
+        <Marquee direction="left" fade={true}>
+            <div>Content 1</div>
+            <div>Content 2</div>
+            <div>Content 3</div>
+        </Marquee>
+    </div>
+	<div class="flex flex-col md:flex-row">
+		<p>hello there</p>
+		<p>hello there</p>
+		<p>hello there</p>
+		<p>hello there</p>
+		<p>hello there</p>
+	</div>
+</div>
